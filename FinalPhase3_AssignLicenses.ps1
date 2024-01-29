d04d1265565431f91cf6762d9189552293e8156e
zafferjoo <48456964+zafferjoo@users.noreply.github.com>
2024-01-29 11:50:51 +0530

    Add files via upload

---
+++ FinalPhase3_AssignLicenses.ps1
@@ -0,0 +1,66 @@
+﻿$Report=import-csv ExtendedLicensesReport.csv
+
+$groups=$Report |Group-Object GroupDisplayName
+foreach($group in $groups){
+ 
+ $licInfo=($Report |?{$_.GroupDisplayName -eq $group.Name}).LicensesID | Group-Object 
+   foreach($lic in $LicInfo)
+   {
+     $disab=$Report|?{$_.GroupDisplayName -eq $group.Name -and $_.LicensesID -eq $Lic.name}
+     $plans=@()
+        foreach($d in $disab)
+           {
+
+               #Write-host " The disabled Plans are for Group $($group.Name)  with Licenses  $($lic.name) are  $($d.DisabledPlans)" -ForegroundColor Green
+               $plans+=$($d.DisabledPlansID)
+
+           }
+
+#========================
+if($plans -match "No Disable")
+{
+$plans=""
+}
+
+
+$params = @{
+	AddLicenses = @(
+		@{
+			DisabledPlans =$plans
+			SkuId = $lic.Name
+		}
+		
+	)
+	RemoveLicenses = @(
+	)
+}
+$y="======================="
+$y
+$group.Values
+$params['addlicenses']
+$group.Name
+
+$y="======================="
+$y
+
+
+#Set-MgGroupLicense -GroupId "89a53ccd-e5d2-4eba-a290-ee09c893ef1a" -BodyParameter $params
+ 
+   }
+   
+ 
+# $LicId=($Report |?{$_.GroupDisplayName -eq $group.Name}).LicensesID
+ #    $disab=$Report|?{$_.GroupDisplayName -eq $group.Name -and $_.LicensesName -eq $Lic}
+
+
+ #$licInfo
+ #$disab
+ #$LicId
+ #$group.Name
+ #$LicId
+ #$Licinfo 
+ 
+ Write-Host "*************************"
+
+
+}
---
+++ Phase1.ps1
@@ -0,0 +1,54 @@
+﻿#https://blog.admindroid.com/auto-assign-licenses-with-group-based-licensing-in-microsoft-365/
+
+$groups=get-msolgroup -All
+Write-host "Total number of Groups created are $($groups.count)"
+$sku=Get-AzureADSubscribedSku
+$gpinfo =$groups|?{$_.AssignedLicenses -ne $null}
+foreach($Gp in $gpinfo)
+{
+ Write-host "Working on Group $($Gp.displayname)"
+ $Licenses=Get-MsolGroup -ObjectId $Gp.Objectid |select -ExpandProperty AssignedLicenses
+ 
+
+foreach($Lic in $Licenses)
+{
+
+   $Info=$lic | select -ExpandProperty AccountSkuId
+   $LicNo=($sku|?{$_.SkuPartNumber -eq $info.SkuPartNumber}).ObjectId.split("_")[1]
+   $sku|?{$_.SkuPartNumber -eq $info.SkuPartNumber}
+   
+  
+
+
+
+   $licensesName=$Info.AccountName + ":" + $Info.SkuPartNumber
+   Write-host "Working on Licenses $licensesName" -ForegroundColor Green
+ 
+   $plans=$lic | select -ExpandProperty disabledserviceplans
+   if($plans.count -gt 0)
+   {
+     foreach($plan in $Plans)
+     {
+      $obj=new-object psobject 
+      $obj |add-member -NotePropertyname GroupDisplayname -NotePropertyValue $($Gp.displayname)
+      $obj |add-member -NotePropertyname LicensesName -NotePropertyValue $licensesName
+      $obj |Add-member -NotePropertyName LicensesID   -NotePropertyValue $LicNo                  
+      $obj |add-member -NotePropertyname DisabledPlans -NotePropertyValue $Plan
+      $obj |Export-csv LicensesReport.csv -Append -nti
+      
+
+     }
+   
+   }
+   else
+   {  
+      $obj=new-object psobject 
+      $obj |add-member -NotePropertyname GroupDisplayname -NotePropertyValue $($Gp.displayname)
+      $obj |add-member -NotePropertyname LicensesName -NotePropertyValue $licensesName
+      $obj |Add-member -NotePropertyName LicensesID   -NotePropertyValue $LicNo                  
+      $obj |add-member -NotePropertyname DisabledPlans -NotePropertyValue ""
+      $obj |Export-csv LicensesReport.csv -Append -nti
+      
+   }
+}
+}
